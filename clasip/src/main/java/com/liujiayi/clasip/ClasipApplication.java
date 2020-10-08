2089f4827a115b1a195362f14b1525ef5aed3fd2
hailicy <2572729959@qq.com>
2020-10-08 20:08:14 +0800

    基本

---
+++ clasip/src/main/java/com/liujiayi/clasip/ClasipApplication.java
@@ -0,0 +1,26 @@
+package com.liujiayi.clasip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+//@SpringBootApplication
+//public class ClasipApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(ClasipApplication.class, args);
+//    }
+//
+//}
+
+@SpringBootApplication
+public class ClasipApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ClasipApplication.class); } public static void main(String[] args) {
+        SpringApplication.run(ClasipApplication.class, args);
+    }
+}
