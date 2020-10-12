669428ea32718290a9da4551c570425d25d2e17c
hailicy <2572729959@qq.com>
2020-10-12 17:02:01 +0800

    配置

--- clasip/src/main/java/com/liujiayi/clasip/ClasipApplication.java
+++ clasip/src/main/java/com/liujiayi/clasip/ClasipApplication.java
@@ -1,7 +1,7 @@
 package com.liujiayi.clasip;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -20,7 +20,10 @@ public class ClasipApplication extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(ClasipApplication.class); } public static void main(String[] args) {
+        return application.sources(ClasipApplication.class);
+    }
+
+    public static void main(String[] args) {
         SpringApplication.run(ClasipApplication.class, args);
     }
 }
