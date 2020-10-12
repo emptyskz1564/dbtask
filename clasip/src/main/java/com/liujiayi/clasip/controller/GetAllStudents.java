1ace73f3c3749ae9a2999836e18f31cba71de08c
hailicy <2572729959@qq.com>
2020-10-12 18:00:06 +0800

    基本

---
+++ clasip/src/main/java/com/liujiayi/clasip/controller/GetAllStudents.java
@@ -0,0 +1,26 @@
+package com.liujiayi.clasip.controller;
+
+import com.liujiayi.clasip.dao.StudentDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 创建时间: 2020-10-12 17:07
+ * 文件备注:
+ * 编写人员: 杨伯益
+ */
+
+@Controller
+public class GetAllStudents {
+
+    @Autowired
+    StudentDao studentDao;
+
+    @RequestMapping("/allstudents")
+    @ResponseBody
+    public Object test1(){
+        return studentDao.selectList(null);
+    }
+}
