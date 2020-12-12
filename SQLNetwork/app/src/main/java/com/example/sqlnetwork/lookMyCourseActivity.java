827a4e8e661c2459049040214f2ae238a5e8cd93
hailicy <2572729959@qq.com>
2020-12-13 00:30:10 +0800

    查询我的课程

---
+++ SQLNetwork/app/src/main/java/com/example/sqlnetwork/lookMyCourseActivity.java
@@ -0,0 +1,14 @@
+package com.example.sqlnetwork;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lookMyCourseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_look_my_course);
+    }
+}
---
+++ clasip/src/main/java/com/liujiayi/clasip/controller/lookMyCourseController.java
@@ -0,0 +1,11 @@
+package com.liujiayi.clasip.controller;
+
+/**
+ * 创建时间: 2020-12-12 23:24
+ * 文件备注:
+ * 编写人员: 杨伯益
+ */
+
+
+public class lookMyCourseController {
+}
